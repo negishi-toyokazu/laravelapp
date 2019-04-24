7fafee0aa0dd4a286d12c4cfa187ecf20bc819a0
negitoyo <negishitoyokazu@toyo-pc-2.local>
2019-04-24 21:32:06 +0900

    Feat:Stripe決済のデモ

---
+++ app/Http/Controllers/ChargeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Stripe\Stripe;
+use Stripe\Customer;
+use Stripe\Charge;
+
+class ChargeController extends Controller
+{
+    /*単発決済用のコード*/
+    public function charge(Request $request)
+    {
+        try {
+            Stripe::setApiKey('sk_test_U4jTkceXK5FQVJo08pI5VEw800DmWU16g5');
+
+            $customer = Customer::create(array(
+                'email' => $request->stripeEmail,
+                'source' => $request->stripeToken
+            ));
+
+            $charge = Charge::create(array(
+                'customer' => $customer->id,
+                'amount' => 1000,
+                'currency' => 'jpy'
+            ));
+
+            return back();
+        } catch (\Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+}
